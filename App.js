b41c53b84c739a4fa5945e0092f8b8f8f060db10
devrodriguesz <dev.emersonrodrigues@outlook.com>
2022-08-22 21:23:51 -0300

    Aplicando ActivyIndicator para esperar aquisicao https

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
 
 import api from './src/services/api';
 import Filmes from './src/Filmes';
 
 export default function App() {
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
 
@@ -13,27 +14,36 @@ export default function App() {
             const response = await api.get('r-api/?api=filmes');
             //console.log(response.data);
             setFilmes(response.data);
+            setLoading(false);
         }
 
         loadFilmes();
 
     }, [])
 
-    
-    
-    return (
-        <View style={styles.container}>
-            <FlatList
-            data={filmes}
-            keyExtractor={ item=> String(item.id) }
-            renderItem={ ({item}) => <Filmes data={item} /> }
-            />
+    if (loading) {
+        return(
+        <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+            <ActivityIndicator color= '#09A6FF' size={45} />
         </View>
-    )
+        )
+    }else {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={filmes}
+                    keyExtractor={item => String(item.id)}
+                    renderItem={({ item }) => <Filmes data={item} />}
+                />
+            </View>
+        )
+    }
+
+
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
     }
 })
